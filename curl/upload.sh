301dae2978fbb25d44df55f3f3507fc286306e47
Christophe Thiebaud <christophe.thiebaud@sap.com>
2013-06-22 00:20:39 +0200

    upgrades
    Change-Id: I43cceeb50ea9bc4b068d78f448ae0755b9de5996

--- curl/upload.sh
+++ curl/upload.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 # https://parimaginep1894179457trial.hanatrial.ondemand.com/parimagine/documents/
 # --noproxy http://localhost:1212/geppaequo-fileupload/documents/
-curl --proxy http://proxy:8080 --verbose -i -F "file=@$1" "https://parimaginep1894179457trial.hanatrial.ondemand.com/parimagine/documents/"`dirname $1`
+#curl --proxy http://proxy:8080 --verbose -i -F "file=@$1" "https://parimaginep1894179457trial.hanatrial.ondemand.com/parimagine/documents/"`dirname $1`
+curl --noproxy --verbose -i -F "file=@$1" "http://localhost:9283/parimagine/documents/"`dirname $1`
