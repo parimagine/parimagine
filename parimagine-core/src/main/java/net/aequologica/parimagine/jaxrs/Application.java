5337e8c1a17ef1be9fb485517c9a7b2a88074fbe
i051108 <christophe.thiebaud@sap.com>
2018-04-25 18:00:26 +0200

    pas convaincu

--- parimagine-core/src/main/java/net/aequologica/parimagine/jaxrs/Application.java
+++ parimagine-core/src/main/java/net/aequologica/parimagine/jaxrs/Application.java
@@ -1,11 +1,14 @@
 package net.aequologica.parimagine.jaxrs;
 
+import javax.ws.rs.ApplicationPath;
+
 import org.glassfish.jersey.logging.LoggingFeature;
 import org.glassfish.jersey.server.ResourceConfig;
 import org.glassfish.jersey.server.mvc.jsp.JspMvcFeature;
 
 import com.fasterxml.jackson.jaxrs.json.JacksonJsonProvider;
 
+@ApplicationPath("/api/parimagine/v1")
 public class Application extends ResourceConfig {
 
     public Application() {
@@ -13,7 +16,7 @@ public class Application extends ResourceConfig {
 		register(LoggingFeature.class);
         register(JacksonJsonProvider.class);
         register(JspMvcFeature.class);
+        
         register(Resource.class);
-
     }
 }
